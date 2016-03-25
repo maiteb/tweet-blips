1a4424413efb0c0b222c9f94a768c96bd66d2511
Maitê Balhester <mbalhest@thoughtworks.com>
2016-03-25 11:26:22 -0300

    Generalizing the hashtag to be found

--- consumer.py
+++ consumer.py
@@ -2,16 +2,27 @@
 
 from twitter import *
 import os
+import argparse
 
-access_key=os.getenv('TWITTER_ACCESS_KEY')
-access_secret=os.getenv('TWITTER_ACCESS_SECRET')
-consumer_key=os.getenv('TWITTER_CONSUMER_KEY')
-consumer_secret=os.getenv('TWITTER_CONSUMER_SECRET')
+def _parse_args():
+    parser = argparse.ArgumentParser("Twitter's Feed Consumer")
+    parser.add_argument('--hashtag',
+                        help='hashtag to be searched WITHOUT the # signal')
+    args = parser.parse_args()
+    return args
 
-t = Twitter(auth=OAuth(access_key,access_secret,consumer_key,consumer_secret))
+if __name__ == '__main__':
+    access_key=os.getenv('TWITTER_ACCESS_KEY')
+    access_secret=os.getenv('TWITTER_ACCESS_SECRET')
+    consumer_key=os.getenv('TWITTER_CONSUMER_KEY')
+    consumer_secret=os.getenv('TWITTER_CONSUMER_SECRET')
 
-query = t.search.tweets(q='#SomeWeirdHashtag')
+    args = _parse_args()
 
-for result in query["statuses"]:
-    print "(%s) @%s     %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
+    t = Twitter(auth=OAuth(access_key,access_secret,consumer_key,consumer_secret))
+
+    query = t.search.tweets(q='#' + args.hashtag)
+
+    for result in query["statuses"]:
+        print "(%s) @%s     %s" % (result["created_at"], result["user"]["screen_name"], result["text"])
 
