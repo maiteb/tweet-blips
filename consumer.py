c6e5eb16d7867eb0a3c30ee9c0c0ec236d13a03b
Maitê Balhester <mbalhest@thoughtworks.com>
2016-03-29 20:14:14 -0300

    Now we are expecting two hashtags - and the code is not that flexible anymore :(

--- consumer.py
+++ consumer.py
@@ -8,8 +8,9 @@ from export_google_sheet import export_all
 
 def _parse_args():
     parser = argparse.ArgumentParser("Twitter's Feed Consumer")
-    parser.add_argument('--hashtag',
-                        help='hashtag to be searched WITHOUT the # signal')
+    parser.add_argument('--hashtags',
+                        nargs='*',
+                        help='2 hashtags to be searched WITHOUT the # signal')
     args = parser.parse_args()
     return args
 
@@ -19,7 +20,9 @@ def _parse_tweets(statuses,max_id):
         current_id = long(result['id_str'])
         if max_id < current_id:
             max_id = current_id
-        tweets.append(Tweet(result['created_at'], result['user']['screen_name'], result['user']['name'],result['text']))
+        tweet = Tweet(result['created_at'], result['user']['screen_name'], result['user']['name'],result['text'])
+        if tweet.blip is not None:
+            tweets.append(tweet)
     return max_id, tweets
 
 def _find_last_id_found():
@@ -47,12 +50,12 @@ if __name__ == '__main__':
     t = Twitter(auth=OAuth(access_key,access_secret,consumer_key,consumer_secret))
 
     last_id_found = long(_find_last_id_found())
-    query = t.search.tweets(q='#' + args.hashtag,count=50, since_id=last_id_found)
+    query = t.search.tweets(q='#' + args.hashtags[0] + "+#" + args.hashtags[1],count=50, since_id=last_id_found)
 
     if not query['statuses']:
-        print "No new tweet was found with the hashtag #" + args.hashtag
+        print "No new tweet was found with the hashtag #" + args.hashtags[0] + " and #" + args.hashtags[1]
     else:
         max_id, tweets =_parse_tweets(query["statuses"], last_id_found)
         _update_max_id(max_id)
         export_all(tweets)
-        print "We exported " + str(len(tweets)) + " new tweets with the hashtag #" + args.hashtag + " to the Google Sheet"
+        print "We exported " + str(len(tweets)) + " new tweets with the hashtag #" + args.hashtags[0] + " and #" + args.hashtags[1] + " to the Google Sheet"
